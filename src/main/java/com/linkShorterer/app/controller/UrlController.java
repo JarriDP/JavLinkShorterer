6f50a1b3500e06be27c3129aabd5a0282cb9d8ae
JarriDP <j.enrique99lopez@gmail.com>
2024-06-24 13:35:04 +0200

    added a function to short the links

--- src/main/java/com/linkShorterer/app/controller/UrlController.java
+++ src/main/java/com/linkShorterer/app/controller/UrlController.java
@@ -4,9 +4,9 @@ package com.linkShorterer.app.controller;
 import com.linkShorterer.app.model.UrlMapping;
 import com.linkShorterer.app.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +24,21 @@ public class UrlController {
 
     @GetMapping("/{url}")
     public UrlMapping getByOriginalUrl(@RequestBody String originalUrl){
-        return urlService.getByOriginalUrl(originalUrl)
+        return urlService.getByOriginalUrl(originalUrl);
+    }
+
+    @PostMapping("/short")
+    public ResponseEntity<String> shortNewUrl (@RequestBody String originalUrl) {
+        if(originalUrl == null || originalUrl.isEmpty()){
+            return ResponseEntity.badRequest().body("Introduce una URL");
+        }
+        try{
+            urlService.generateShorteredUrl(originalUrl);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("Error al acortar la url");
+        }
+
     }
 
 }
